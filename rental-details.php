dd9c04e58e6f68eafa8929672c24dc7c9a0a3e71
Alexis-Duru <alexis.duru.pro@gmail.com>
2021-05-05 11:01:55 +0200

    PROFIL / LOGIN / RENTAL / RENTAL DETAILS PHP OK

--- rental-details.php
+++ rental-details.php
@@ -1,5 +1,14 @@
-<?php require 'inc/header.php' ?>
+<?php require 'inc/header.php'; ?>
 
+<?php
+
+
+$id = $_GET['id'];
+
+$sqlRentals = "SELECT r.*, u.username, c.categories_name FROM rentals AS r LEFT JOIN users AS u ON r.author = u.id LEFT JOIN categories AS c ON r .rental_category = c.categories_id WHERE r.rental_id = {$id} ";
+
+$rentals = $connect->query($sqlRentals)->fetch(PDO::FETCH_ASSOC);
+?>
     <section id="header-banner">
         <div id="header-banner-container">
             <div>
@@ -15,26 +24,31 @@
             </div>
             <div id="container-in-2">
                 <div>
-                    <h2>STULIDAY #1</h2>
+                    <h2><?php echo $rentals['rental_name']; ?></h2>
                     <div class="price">
-                        <p class="amount">25€</p>
+                        <p class="amount"><?php echo $rentals['rental_price']; ?>$</p>
                         <p> / </p>
                         <h4>NIGHT</h4>
                     </div>
                 </div>
                 <span></span>
-                <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                    dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                    asperiores amet sapiente voluptatum est nisi totam soluta.</p>
+                <p>Category : <?php echo $rentals['categories_name']; ?></p>
+                <span></span>
+                <p><?php echo $rentals['rental_description']; ?></p>
             </div>
             <div id="container-in-3">
                 <span></span>
                 <img src="assets/img/image/details-rental-2.jpeg">
             </div>
+            
             <div id="container-in-4">
                 <span></span>
                 <a href="#">BOOK</a>
             </div>
+            <span></span>
+                <p>Published on: <?php echo $rentals['created_at']; ?>
+            </p>
+            
         </div>
     </section>
 
--- rental.php
+++ rental.php
@@ -1,21 +1,10 @@
-<?php require 'inc/header.php';
-// $title = 'Rental - Le Chouette Coin';
+<?php require 'inc/header.php' ?>
+<?php
 
-
-// $sql = 'SELECT * FROM categories';
-// $res = $conn->query($sql);
-// $categories = $res->fetchAll();
-
-// if (isset($_POST['search_form'])) {
-//     $category = intval(strip_tags($_POST['rental_category']));
-//     $search_text = strip_tags($_POST['search_text']);
-
-//     $sql2 = "SELECT * FROM rentals WHERE rental_category LIKE '%{$category}%' AND rental_name LIKE '%{$search_text}%'";
-//     $res2 = $conn->query($sql2);
-//     $search = $res2->fetchAll();
-// }
+// 
+$sqlRentals = "SELECT r.*, u.username, c.categories_name FROM rentals AS r LEFT JOIN users AS u ON r.author = u.id LEFT JOIN categories AS c ON r.rental_category = c.categories_id";
+$rentals = $connect->query($sqlRentals)->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 
     <section id="header-banner">
         <div id="header-banner-container">
@@ -27,229 +16,37 @@
 
     <section id="rental-container">
         <div id="container-in">
-
+            <?php
+            foreach ($rentals as $rental) {
+            ?>
                 <div class="rental">
                     <img src="assets/img/image/home/3.jpg">
                     <div>
-                        <h3>STULIDAY #1<?php echo $rental['rental_name']; ?></h3>
-                        <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                            dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                            asperiores amet sapiente voluptatum est nisi totam soluta.<?php echo $product['rental_description']; ?></p>
+                        <h3><?php echo $rental['rental_name']; ?></h3>
+                        <p><?php echo $rental['rental_description']; ?></p>
                         <div class="price">
-                            <p class="amount">25€<?php echo $product['rental_price']; ?></p>
+                            <p class="amount"><?php echo $rental['rental_price']; ?>$</p>
                             <p> / </p>
                             <h4>NIGHT</h4>
                         </div>
 
                         <div id="btn-details-rental">
-                            <p><a href="rental-details.php">details</a></p>
+                            <p><a href="rental-details.php?id=<?php echo $rental['rental_id']; ?>" >details</a></p>
                         </div>
+                        
+
+                        
+                        <p><?php echo $rental['categories_name']; ?></p>
+                        <p><?php echo $rental['created_at']; ?></p>
                         <img id="etoile" src="assets/img/icones/star.png">
                         <img id="etoile" src="assets/img/icones/star.png">
                         <img id="etoile" src="assets/img/icones/star.png">
                         <img id="etoile" src="assets/img/icones/star.png">
                     </div>
                 </div>
-
-
-
-
-                <div class="rental">
-                    <img src="assets/img/image/home/3.jpg">
-                    <div>
-                        <h3>STULIDAY #1<?php echo $rental['rental_name']; ?></h3>
-                        <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                            dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                            asperiores amet sapiente voluptatum est nisi totam soluta.<?php echo $product['rental_description']; ?></p>
-                        <div class="price">
-                            <p class="amount">25€<?php echo $product['rental_price']; ?></p>
-                            <p> / </p>
-                            <h4>NIGHT</h4>
-                        </div>
-
-                        <div id="btn-details-rental">
-                            <p><a href="rental-details.php">details</a></p>
-                        </div>
-                        <img id="etoile" src="assets/img/icones/star.png">
-                        <img id="etoile" src="assets/img/icones/star.png">
-                        <img id="etoile" src="assets/img/icones/star.png">
-                        <img id="etoile" src="assets/img/icones/star.png">
-                    </div>
-                </div>
-
-            <!-- <div class="rental">
-                <img src="assets/img/image/home/4.jpg">
-                <div>
-                    <h3>STULIDAY #2</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                    <div class="price">
-                        <p class="amount">30€</p>
-                        <p> / </p>
-                        <h4>NIGHT</h4>
-                    </div>
-                    <div id="btn-details-rental">
-                        <p><a href="rental-details.php">details</a></p>
-                    </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-            
-            <div class="rental">
-                <img src="assets/img/image/home/5.jpg">
-                <div>
-                    <h3>STULIDAY #3</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                    <div class="price">
-                        <p class="amount">13€</p>
-                        <p> / </p>
-                        <h4>NIGHT</h4>
-                    </div>
-                    <div id="btn-details-rental">
-                        <p><a href="rental-details.php">details</a></p>
-                    </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-
-            <div class="rental">
-                <img src="assets/img/image/home/6.jpg">
-                <div>
-                    <h3>STULIDAY #4</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                    <div class="price">
-                        <p class="amount">15€</p>
-                        <p> / </p>
-                        <h4>NIGHT</h4>
-                    </div>
-                    <div id="btn-details-rental">
-                        <p><a href="rental-details.php">details</a></p>
-                    </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-
-            <div class="rental">
-                <img src="assets/img/image/home/7.jpg">
-                <div>
-                    <h3>STULIDAY #5</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                        <div class="price">
-                            <p class="amount">12€</p>
-                            <p> / </p>
-                            <h4>NIGHT</h4>
-                        </div>
-                        <div id="btn-details-rental">
-                            <p><a href="rental-details.php">details</a></p>
-                        </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-
-            <div class="rental">
-                <img src="assets/img/image/home/8.jpg">
-                <div>
-                    <h3>STULIDAY #6</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                    <div class="price">
-                        <p class="amount">25€</p>
-                        <p> / </p>
-                        <h4>NIGHT</h4>
-                    </div>
-                    <div id="btn-details-rental">
-                        <p><a href="rental-details.php">details</a></p>
-                    </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-
-            <div class="rental">
-                <img src="assets/img/image/home/1.jpg">
-                <div>
-                    <h3>STULIDAY #7</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                        <div class="price">
-                            <p class="amount">20€</p>
-                            <p> / </p>
-                            <h4>NIGHT</h4>
-                        </div>
-                        <div id="btn-details-rental">
-                            <p><a href="rental-details.php">details</a></p>
-                        </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-
-            <div class="rental">
-                <img src="assets/img/image/home/2.jpg">
-                <div>
-                    <h3>STULIDAY #8</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                    <div class="price">
-                        <p class="amount">25€</p>
-                        <p> / </p>
-                        <h4>NIGHT</h4>
-                    </div>
-                    <div id="btn-details-rental">
-                        <p><a href="rental-details.php">details</a></p>
-                    </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div>
-
-            <div class="rental">
-                <img src="assets/img/image/home/9.jpg">
-                <div>
-                    <h3>STULIDAY #9</h3>
-                    <p>Description : Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus laboriosam
-                        dolorum sed corporis eligendi facere facilis explicabo placeat sint eveniet. Qui alias
-                        asperiores amet sapiente voluptatum est nisi totam soluta.</p>
-                    <div class="price">
-                        <p class="amount">25€</p>
-                        <p> / </p>
-                        <h4>NIGHT</h4>
-                    </div>
-                    <div id="btn-details-rental">
-                        <p><a href="rental-details.php">details</a></p>
-                    </div>
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                    <img id="etoile" src="assets/img/icones/star.png">
-                </div>
-            </div> -->
+            <?php
+            }
+            ?>
         </div>
     </section>
 
